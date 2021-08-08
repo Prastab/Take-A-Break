d71155117e16d48142ef327ef0d96ee87daf8cbe
Prastab <prastabg16@gmail.com>
2021-08-08 15:04:50 +0545

    Added checkboxes

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-$('a.button').click(function(e) {
-    e.preventDefault();
-    window.open('https://www.youtube.com');
-    window.open('http://twitter.com');
-});
 function openLink()
 {
-    window.open('https://github.com');
-    window.open('https://discord.com/app');
-    window.open('https://www.instagram.com');
-    window.open('https://www.twitter.com');
-    window.open('https://www.reddit.com');
-    window.open('https://www.youtube.com');
+    var favorite = [];
+    $.each($("input[type='checkbox']:checked"), function(){
+        favorite.push($(this).val());
+    });
+    
+    console.log(favorite.length);
+    favorite.forEach(element => {
+        window.open(element);
+    });
  
 }
