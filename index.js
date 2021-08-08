41f744044462f571cf27da1524c7082bee0191c6
Prastab <prastabg16@gmail.com>
2021-08-08 12:02:46 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+$('a.button').click(function(e) {
+    e.preventDefault();
+    window.open('https://www.youtube.com');
+    window.open('http://twitter.com');
+});
+function openLink()
+{
+    window.open('https://github.com');
+    window.open('https://discord.com/app');
+    window.open('https://www.instagram.com');
+    window.open('https://www.twitter.com');
+    window.open('https://www.reddit.com');
+    window.open('https://www.youtube.com');
+ 
+}
